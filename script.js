c0a5cd2e1bf5cacb3ebd45489b1c1afb53794d00
oftools <93421519+oftools@users.noreply.github.com>
2021-11-20 20:34:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.addEventListener('scroll',function(){
-const header = document.querySelector('header');
-header.classList.toggle("sticky",window.scrollY > 10);
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky",window.scrollY > 10);
 })
+
+VanillaTilt.init(document.querySelectorAll(".card"),{
+    max: 20,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5,
+});
